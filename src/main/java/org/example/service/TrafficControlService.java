3aa9af6387e53531b0e1eebae31bbdb144a41707
Aministrator <crucin.patricia@yahoo.com>
2026-01-14 08:47:45 +0200

    Task 1

---
+++ src/main/java/org/example/service/TrafficControlService.java
@@ -0,0 +1,47 @@
+package org.example.service;
+
+import org.example.model.*;
+import org.example.repository.VehicleRepository;
+import org.example.repository.FineRepository;
+import org.example.repository.TrafficEventRepository;
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class TrafficControlService {
+    private List<Vehicle> vehicles;
+    private List<Fine> fines;
+    private List<TrafficEvent> events;
+    private final VehicleRepository vehicleRepository;
+    private final FineRepository fineRepository;
+    private final TrafficEventRepository trafficEventRepository;
+
+    public TrafficControlService() {this.vehicleRepository = new VehicleRepository();
+    this.fineRepository = new FineRepository();
+    this.trafficEventRepository = new TrafficEventRepository();
+    }
+
+    public void loadData(){
+        this.vehicles = vehicleRepository.loadVehicles("vehicles.json");
+        this.events = trafficEventRepository.loadTrafficEvents("events.json");
+        this.fines = fineRepository.loadFines("fines.json");
+
+        System.out.println("Vehicles loaded:" + vehicles.size());
+        System.out.println("Fines loaded:" + fines.size());
+        System.out.println("Traffic events loaded:" + events.size());
+    }
+
+    public void printAllVehicles(){
+        for (Vehicle vehicle : vehicles){
+            System.out.println(vehicle);
+        }
+    }
+
+    //public void filterByVehicleTypeAndStatus
+
+}
